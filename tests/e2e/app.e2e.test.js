bad99a3ba054611efc4caab79029b0dd0369ae4c
Erica <erica.friberg89@gmail.com>
2026-04-01 12:15:49 +0200

    added tests and gitignore

---
+++ tests/e2e/app.e2e.test.js
@@ -0,0 +1,5 @@
+import { test, expect } from "@playwright/test";
+
+test("full flow: load posts and tags on start", async ({ page }) => {
+
+});
---
+++ tests/unit/model.test.js
@@ -0,0 +1,10 @@
+import { describe, test, expect, beforeEach } from "vitest";
+
+describe("todo-model", () => {
+    beforeEach(() => {
+    });
+
+    test("toggleTodo with an invalid id does nothing", () => {
+
+    })
+});
