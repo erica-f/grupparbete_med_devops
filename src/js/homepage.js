b906a387ccc61aebc6aec07f6fb97d4d247f65ec
Carolina <carolina.warntorp@outlook.com>
2026-04-05 19:53:10 +0200

    ref change icon

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -30,7 +30,7 @@ const saveChoices = (u) => {
   radios.forEach((radio) => {
     radio.checked = false;
   });
-  window.location.href = "/training.html";
+  // window.location.href = "/training.html";
 };
 
 const checkForm = () => {
@@ -227,7 +227,7 @@ export const choosetrainingConditionsHTML = (u) => {
   grid3.append(equipmentInput);
 
   const equipmentLabel = document.createElement("label");
-  equipmentLabel.htmlFor = "equipment";
+  equipmentLabel.htmlFor = "with-equipment";
   equipmentLabel.className = "option-card";
   grid3.append(equipmentLabel);
 
@@ -254,7 +254,7 @@ export const choosetrainingConditionsHTML = (u) => {
   grid3.append(noEquipmentInput);
 
   const noEquipmentLabel = document.createElement("label");
-  noEquipmentLabel.htmlFor = "equipment";
+  noEquipmentLabel.htmlFor = "no-equipment";
   noEquipmentLabel.className = "option-card";
   grid3.append(noEquipmentLabel);
 
@@ -348,7 +348,7 @@ export const choosetrainingConditionsHTML = (u) => {
   const btn = document.createElement("button");
   btn.id = "trainingBtn";
   btn.className = "btn btn--primary";
-  btn.innerText = "Skapa mitt pass ✨";
+  btn.innerHTML = `Skapa mitt pass <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkles-icon lucide-sparkles"><path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z"/><path d="M20 2v4"/><path d="M22 4h-4"/><circle cx="4" cy="20" r="2"/></svg>`;
   btn.disabled = true;
   btn.addEventListener("click", () => saveChoices(u));
 
