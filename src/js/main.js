5c4104953d4daf3d730cc6f20bed52cf39d553cc
Carolina <carolina.warntorp@outlook.com>
2026-04-06 22:25:01 +0200

    ref add commented code

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 import { chooseUserHTML } from "./homepage";
 
 chooseUserHTML();
+
+// import { choosetrainingConditionsHTML, chooseUserHTML } from "./homepage";
+// const userInLS = JSON.parse(localStorage.getItem("fitParent"));
+// const activeTrainer = userInLS && userInLS.is_active ? userInLS.user : false;
+// {
+//   activeTrainer === false
+//     ? chooseUserHTML()
+//     : choosetrainingConditionsHTML(userInLS.user);
+// }
