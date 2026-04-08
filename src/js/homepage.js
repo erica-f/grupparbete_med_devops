290a24412a267d2264f7a8f6db9a3846d7b4577b
Carolina <carolina.warntorp@outlook.com>
2026-04-08 15:57:56 +0200

    ref add colorfn and dataset on card

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -2,12 +2,15 @@ import { getUsers } from "../api/getDataFns.js";
 import { createUser } from "../api/createDataFns.js";
 // import [storeExerciseSettings] from "../"
 
-const greenCSSVariable = getComputedStyle(document.documentElement)
-  .getPropertyValue("--color-green")
-  .trim();
-const salmonCSSVariable = getComputedStyle(document.documentElement)
-  .getPropertyValue("--color-accent")
-  .trim();
+const getColorsFromCSS = () => {
+  const green = getComputedStyle(document.documentElement)
+    .getPropertyValue("--color-green")
+    .trim();
+  const salmon = getComputedStyle(document.documentElement)
+    .getPropertyValue("--color-accent")
+    .trim();
+  return { green, salmon };
+};
 
 const saveChoices = (u) => {
   const selectedTime = document.querySelector('input[name="time"]:checked');
@@ -47,7 +50,7 @@ const saveChoices = (u) => {
   window.location.href = "/training.html";
 };
 
-const checkForm = () => {
+export const checkForm = () => {
   const time = document.querySelector('input[name="time"]:checked');
   const location = document.querySelector('input[name="location"]:checked');
   const equipment = document.querySelector('input[name="equipment"]:checked');
@@ -113,7 +116,7 @@ export const choosetrainingConditionsHTML = (u) => {
 
   const label1 = document.createElement("div");
   label1.className = "section-label";
-  label1.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke=${greenCSSVariable} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock text-primary"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg> Hur länge kan du träna?`;
+  label1.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke=${getColorsFromCSS().green} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clock text-primary"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg> Hur länge kan du träna?`;
   section1.append(label1);
 
   const grid1 = document.createElement("div");
@@ -169,7 +172,7 @@ export const choosetrainingConditionsHTML = (u) => {
 
   const label2 = document.createElement("div");
   label2.className = "section-label";
-  label2.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke=${greenCSSVariable} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house text-primary"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg> Var vill du träna?`;
+  label2.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke=${getColorsFromCSS().green} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house text-primary"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg> Var vill du träna?`;
   section2.append(label2);
 
   const grid2 = document.createElement("div");
@@ -239,7 +242,7 @@ export const choosetrainingConditionsHTML = (u) => {
 
   const label3 = document.createElement("div");
   label3.className = "section-label";
-  label3.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke=${greenCSSVariable} stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-dumbbell text-primary"><path d="M14.4 14.4 9.6 9.6"></path><path d="M18.657 21.485a2 2 0 1 1-2.829-2.828l-1.767 1.768a2 2 0 1 1-2.829-2.829l6.364-6.364a2 2 0 1 1 2.829 2.829l-1.768 1.767a2 2 0 1 1 2.828 2.829z"></path><path d="m21.5 21.5-1.4-1.4"></path><path d="M3.9 3.9 2.5 2.5"></path><path d="M6.404 12.768a2 2 0 1 1-2.829-2.829l1.768-1.767a2 2 0 1 1-2.828-2.829l2.828-2.828a2 2 0 1 1 2.829 2.828l1.767-1.768a2 2 0 1 1 2.829 2.829z"></path></svg> Ska redskap användas?`;
+  label3.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke=${getColorsFromCSS().green} stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-dumbbell text-primary"><path d="M14.4 14.4 9.6 9.6"></path><path d="M18.657 21.485a2 2 0 1 1-2.829-2.828l-1.767 1.768a2 2 0 1 1-2.829-2.829l6.364-6.364a2 2 0 1 1 2.829 2.829l-1.768 1.767a2 2 0 1 1 2.828 2.829z"></path><path d="m21.5 21.5-1.4-1.4"></path><path d="M3.9 3.9 2.5 2.5"></path><path d="M6.404 12.768a2 2 0 1 1-2.829-2.829l1.768-1.767a2 2 0 1 1-2.828-2.829l2.828-2.828a2 2 0 1 1 2.829 2.828l1.767-1.768a2 2 0 1 1 2.829 2.829z"></path></svg> Ska redskap användas?`;
   section3.append(label3);
 
   const grid3 = document.createElement("div");
@@ -309,7 +312,7 @@ export const choosetrainingConditionsHTML = (u) => {
 
   const label4 = document.createElement("div");
   label4.className = "section-label";
-  label4.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke=${salmonCSSVariable} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-baby text-secondary"><path d="M9 12h.01"></path><path d="M15 12h.01"></path><path d="M10 16c.5.3 1.2.5 2 .5s1.5-.2 2-.5"></path><path d="M19 6.3a9 9 0 0 1 1.8 3.9 2 2 0 0 1 0 3.6 9 9 0 0 1-17.6 0 2 2 0 0 1 0-3.6A9 9 0 0 1 12 3c2 0 3.5 1.1 3.5 2.5s-.9 2.5-2 2.5c-.8 0-1.5-.4-1.5-1"></path></svg> Har du barn med dig?`;
+  label4.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke=${getColorsFromCSS().salmon} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-baby text-secondary"><path d="M9 12h.01"></path><path d="M15 12h.01"></path><path d="M10 16c.5.3 1.2.5 2 .5s1.5-.2 2-.5"></path><path d="M19 6.3a9 9 0 0 1 1.8 3.9 2 2 0 0 1 0 3.6 9 9 0 0 1-17.6 0 2 2 0 0 1 0-3.6A9 9 0 0 1 12 3c2 0 3.5 1.1 3.5 2.5s-.9 2.5-2 2.5c-.8 0-1.5-.4-1.5-1"></path></svg> Har du barn med dig?`;
   section4.append(label4);
 
   const grid4 = document.createElement("div");
@@ -439,9 +442,10 @@ export const chooseUserHTML = async () => {
   users.map((u) => {
     const card = document.createElement("div");
     card.className = "option-card";
+    card.dataset.userId = u.id;
 
     const icon = document.createElement("div");
-    icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke=${greenCSSVariable} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user text-primary-foreground"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>`;
+    icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke=${getColorsFromCSS().green} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user text-primary-foreground"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>`;
 
     card.prepend(icon);
     const name = document.createElement("div");
