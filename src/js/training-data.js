fa35be804ddd7cac39607566052737f3ccf97f55
Erica <erica.friberg89@gmail.com>
2026-04-09 14:23:22 +0200

    fixed tests, cleaned css and some code, added sorting to getExercises

--- src/js/training-data.js
+++ src/js/training-data.js
@@ -24,7 +24,6 @@ export async function getData() {
         selectedExercises: selectedExercises,
         getRepAmount: getRepAmount,
     }
-    console.log(savedsettings);
     return savedsettings;
 }
 
